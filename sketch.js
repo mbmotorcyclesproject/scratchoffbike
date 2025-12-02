fd49f208b952b3e7878b89a25c3d32bdf747047a
mbmotorcyclesproject <myrtlebeachmotorcycleproject@gmail.com>
2025-12-02 11:31:54 -0500

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,35 @@
+let topLayer;
+let motorcycle;
+
+function preload() {
+  motorcycle = loadImage("motorcycle.png");
+}
+
+function setup() {
+  createCanvas(motorcycle.width, motorcycle.height);
+
+  
+  topLayer = createGraphics(width, height);
+  topLayer.background(200); 
+
+
+  topLayer.textAlign(CENTER, CENTER);
+  topLayer.textSize(48);
+  topLayer.fill(0);
+  topLayer.text("Myrtle Beach Motorcycle Project", width / 2, height / 2);
+}
+
+function draw() {
+ 
+  image(motorcycle, 0, 0);
+
+
+  image(topLayer, 0, 0);
+}
+
+function mouseDragged() {
+
+  topLayer.erase();
+  topLayer.circle(mouseX, mouseY, 80);
+  topLayer.noErase();
+}
